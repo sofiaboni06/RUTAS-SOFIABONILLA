221abd0d2539e6086c078ae485c5d4431fa009cd
miguel08castro <miguelamgelcastro200400@gmail.com>
2026-02-27 09:25:24 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Navbar from './Navbar'
+
+function App() {
+  
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
